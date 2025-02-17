19afdc99a020ecab2f1d18531e8d780d347c1242
Juan Serna <juanserna427@gmail.com>
2025-02-17 01:00:22 -0500

    Fix in names

--- client/src/AccountNav.jsx
+++ client/src/AccountNav.jsx
@@ -16,7 +16,7 @@ export default function AccountNav() {
     return classes;
   }
   return (
-    <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
+    <nav className="w-full flex justify-center mt-8 gap-1 mb-8">
       <Link className={linkClasses("profile")} to={"/account"}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -32,7 +32,7 @@ export default function AccountNav() {
             d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
           />
         </svg>
-        My profile
+        My Profile
       </Link>
       <Link className={linkClasses("bookings")} to={"/account/bookings"}>
         <svg
@@ -49,7 +49,7 @@ export default function AccountNav() {
             d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
           />
         </svg>
-        My bookings
+        My Bookings
       </Link>
       <Link className={linkClasses("places")} to={"/account/places"}>
         <svg
@@ -66,7 +66,7 @@ export default function AccountNav() {
             d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819"
           />
         </svg>
-        My accommodations
+        My Accommodations
       </Link>
       <Link className={linkClasses("details")} to={"/host/details"}>
         <svg
@@ -90,7 +90,7 @@ export default function AccountNav() {
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M8.242 5.992h12m-12 6.003H20.24m-12 5.999h12M4.117 7.495v-3.75H2.99m1.125 3.75H2.99m1.125 0H5.24m-1.92 2.577a1.125 1.125 0 1 1 1.591 1.59l-1.83 1.83h2.16M2.99 15.745h1.125a1.125 1.125 0 0 1 0 2.25H3.74m0-.002h.375a1.125 1.125 0 0 1 0 2.25H2.99" />
         </svg>
-        My perks
+        My Perks
       </Link>
     </nav>
   );
